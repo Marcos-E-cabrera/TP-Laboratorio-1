e6746152a7668df150bb7119f28142427fc046db
marcos <marcoscabrera021@gmail.com>
2022-11-24 19:29:01 -0300

    subiendo tp4

---
+++ TP-[4]/src/main.c
@@ -0,0 +1,91 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2018>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/LinkedList.h"
+
+
+int main(void)
+{
+	startTesting(1);  // ll_newLinkedList  ( ! ) TERMINADO
+	startTesting(2);  // ll_len  ( ! ) TERMINADO
+	startTesting(3);  // getNode - test_getNode  ( ! ) TERMINADO
+	startTesting(4);  // addNode - test_addNode  ( ! ) TERMINADO
+	startTesting(5);  // ll_add  ( ! ) TERMINADO
+	startTesting(6);  // ll_get  ( ! ) TERMINADO
+	startTesting(7);  // ll_set  ( ! ) TERMINADO
+	startTesting(8);  // ll_remove  ( ! ) TERMINADO
+	startTesting(9);  // ll_clear  ( ! ) TERMINADO
+	startTesting(10); // ll_deleteLinkedList  ( ! ) TERMINADO
+	startTesting(11); // ll_indexOf  ( ! ) TERMINADO
+	startTesting(12); // ll_isEmpty  ( ! ) TERMINADO
+	startTesting(13); // ll_push  ( ! ) TERMINADO
+	startTesting(14); // ll_pop  ( ! ) TERMINADO
+	startTesting(15); // ll_contains  ( ! ) TERMINADO
+	startTesting(16); // ll_containsAll  ( ! ) TERMINADO
+	startTesting(17); // ll_subList  ( ! ) TERMINADO
+	startTesting(18); // ll_clone  ( ! ) TERMINADO
+	startTesting(19); // ll_sort  ( ! ) TERMINADO
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
